a6c61c50c91fcd15bec4714e9e7d795caf604a33
Ben <ben.danielgs@gmail.com>
2020-05-07 15:48:26 +0100

    testing of setDatabaseUtilites and getInstance was successful

--- datasource/src/datasource/DatabaseUtilities.java
+++ datasource/src/datasource/DatabaseUtilities.java
@@ -10,48 +10,51 @@ public class DatabaseUtilities {
     private static DatabaseUtilities databaseUtilities;
     private Connection conn;
 
-    private static final String DB_NAME = "secure-messenger-relay";
+    private static final String DB_NAME = "secure_messenger_relay";
     private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/?useSSL=false";
 
-    private static final String CREATE_DB = "CREATE DATABASE IF NOT EXISTS" + DB_NAME;
+    private static final String CREATE_DB = "CREATE DATABASE IF NOT EXISTS " + DB_NAME;
     private static final String USE_DB = "USE " + DB_NAME;
+
     private static final String CREATE_CONTACTS = "CREATE TABLE IF NOT EXISTS contacts(cid CHAR(88) PRIMARY KEY, alias VARCHAR(255) NOT NULL)";
     private static final String CREATE_NETWORKS = "CREATE TABLE IF NOT EXISTS networks(nid INTEGER PRIMARY KEY, fingerprint CHAR(88) UNIQUE NOT NULL," +
                                                   "port INTEGER UNIQUE NOT NULL, network_alias VARCHAR(255))";
     private static final String CREATE_CHATROOMS = "CREATE TABLE IF NOT EXISTS chatrooms(rid INTEGER PRIMARY KEY, room_alias VARCHAR(255) NOT NULL)";
     private static final String CREATE_NETWORK_CONTACTS  = "CREATE TABLE IF NOT EXISTS networkContacts( nid INTEGER, cid CHAR(88), PRIMARY KEY(nid,cid), " +
-                                                           "FOREIGN KEY(nid) REFERENCES networks(nid), FOREIGN KEY(cid) REFERENCES contacts(cid)";
+                                                           "FOREIGN KEY(nid) REFERENCES networks(nid), FOREIGN KEY(cid) REFERENCES contacts(cid))";
     private static final String CREATE_CHATROOM_CONTACTS = "CREATE TABLE IF NOT EXISTS chatroomContacts( rid INTEGER, cid CHAR(88), PRIMARY KEY(rid,cid), " +
-                                                           "FOREIGN KEY(rid) REFERENCES chatrooms(rid), FOREIGN KEY(cid) REFERENCES contacts(cid)";
+                                                           "FOREIGN KEY(rid) REFERENCES chatrooms(rid), FOREIGN KEY(cid) REFERENCES contacts(cid))";
 
-    private DatabaseUtilities(String username, String password){
+    private DatabaseUtilities(String username, String password) throws SQLException{
         openConnection(username, password);
+        setupPreparedStatements();
     }
 
-    public static void setDatabaseUtilities(String username, String password){
+
+    // NOT SURE WHETHER TO HANDLE EXCEPTION HERE OR NOT
+    public static void setDatabaseUtilities(String username, String password) throws SQLException{
         if(databaseUtilities == null)
-        databaseUtilities = new DatabaseUtilities(username, password);
+                databaseUtilities = new DatabaseUtilities(username, password);
     }
 
-    public static DatabaseUtilities getDatabaseUtilities(){
+    public static DatabaseUtilities getInstance(){
             return databaseUtilities;
     }
 
-    private void openConnection(String username, String password){
-        try{
+    private void openConnection(String username, String password) throws SQLException {
+
             conn = DriverManager.getConnection(CONNECTION_STRING, username, password);
             if(conn != null){
                 setupDatabase();
             }
-        } catch (SQLException e){}
+
     }
 
     /**
      * sets up tables which do not already exist
      */
-    private void setupDatabase() {
+    private void setupDatabase() throws SQLException{
 
-        try {
             Statement statement = conn.createStatement();
 
             statement.execute(CREATE_DB);
@@ -65,15 +68,12 @@ public class DatabaseUtilities {
 
             statement.executeBatch();
 
-        } catch (SQLException e) {
-            System.out.println("Failed to setup database: " + e.getMessage());
-        }
     }
 
     /**
      * Closes the connection when the application closes
      */
-    private void closeConnection() {
+    private void closeConnection(){
 
         try {
 //            if (queryInsertContact != null) {
@@ -88,6 +88,17 @@ public class DatabaseUtilities {
         } catch (SQLException e) {
             System.out.println("Failed to close connection: " + e.getMessage());
         }
+    }
+
+    /**
+     * Setup query prepared statements
+     */
+    private void setupPreparedStatements() throws SQLException{
+
+//            queryInsertContact = conn.prepareStatement(INSERT_CONTACT);
+//            queryInsertAccount = conn.prepareStatement(INSERT_ACCOUNT);
+
+
 
     }
 }
--- datasource/src/datasource/DatabaseUtilitiesTest.java
+++ datasource/src/datasource/DatabaseUtilitiesTest.java
@@ -1,14 +1,33 @@
 package datasource;
 
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.sql.SQLException;
+import static org.junit.Assert.*;
+
 public class DatabaseUtilitiesTest {
 
-    @org.junit.Test
-    public void setDatabaseUtilities() {
+    private static DatabaseUtilities databaseUtilities;
 
+    @BeforeClass
+    public static void setUp() {
+        try {
+            DatabaseUtilities.setDatabaseUtilities("relay-app", "relaypass");
+            databaseUtilities = DatabaseUtilities.getInstance();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     @org.junit.Test
-    public void getDatabaseUtilities() {
+    public void setDatabaseUtilities() {
+            assertNotNull(databaseUtilities);
+    }
+
+    @org.junit.Test
+    public void getInstance() {
+        assertNotNull(DatabaseUtilities.getInstance());
     }
 }
